406e6b0a9d45e9ca5500769d64e2c86d4c28f6cc
Artur Aghajanyan <artur.aghajanyan1997@mail.ru>
2021-12-10 23:54:42 +0400

    Created shell sort with c++

---
+++ Shell/shell.cpp
@@ -0,0 +1,40 @@
+#include  <iostream>
+
+int shellSort(int arr[], int n){
+    for (int gap = n/2; gap > 0; gap /= 2){
+        for (int i = gap; i < n; i += 1)
+        {
+            int temp = arr[i];
+            int j;           
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                arr[j] = arr[j - gap];
+            
+            arr[j] = temp;
+        }
+    }
+    return 0;
+}
+ 
+void printArray(int arr[], int n){
+    for (int i=0; i<n; i++)
+        std::cout << arr[i] << " ";
+
+}
+ 
+int main(){
+    int arr[] = {12, 34, 54, 2, 3}, i;
+    int size = sizeof(arr)/sizeof(arr[0]);
+ 
+    std::cout << "Before sorting: \n";
+    for (int i=0; i<size; i++)
+        std::cout << arr[i] << " ";
+
+    shellSort(arr, size);
+    std::cout << "\n\n**********\n";
+    std::cout << "\nAfter sorting: \n";
+    
+    for (int i=0; i<size; i++)
+        std::cout << arr[i] << " ";
+    std::cout<<std::endl;
+    return 0;
+}
